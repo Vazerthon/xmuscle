ef2e2fb87fb9bba717b919365d48a04ae3faf1a1
Paul Vallis <vallis1@gmail.com>
2020-01-03 14:56:36 +0000

    Remove boilerplate from client

--- web/src/components/Home.jsx
+++ web/src/components/Home.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import { useQuery } from '@apollo/react-hooks';
 import { styled, media } from './styling';
-
-import boilerplate from '../graphql/queries/boilerplate';
 
 const Container = styled.div`
   margin: ${({ theme }) => theme.spacing.units(4)};
@@ -17,19 +14,9 @@ const Container = styled.div`
 `;
 
 export default function Home() {
-  const { loading, error, data } = useQuery(boilerplate);
-
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-
-  if (error) {
-    return <p>Error :(</p>;
-  }
-
   return (
     <Container>
-      {data.boilerplate.name}
+      Hello, world
     </Container>
   );
 }
