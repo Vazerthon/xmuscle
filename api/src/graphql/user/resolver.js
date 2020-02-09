53615c70cece05787878fc9fb126472144da8bb6
Paul Vallis <vallis1@gmail.com>
2020-02-09 20:19:17 +0000

    Define login/register flow

--- api/src/graphql/user/resolver.js
+++ api/src/graphql/user/resolver.js
@@ -1,6 +1,7 @@
 // eslint-disable-next-line import/prefer-default-export
 export const resolver = {
   Mutation: {
+    register: (_, { code }, { services }) => services.auth.register(code),
     login: (_, { code }, { services }) => services.auth.login(code),
   },
 };
--- api/src/service/auth.js
+++ api/src/service/auth.js
@@ -2,6 +2,7 @@ import Boom from '@hapi/boom';
 import { compose, prop } from 'ramda';
 import tokenService from './token';
 import dataQuery from '../data/queries';
+import dataMutation from '../data/mutations';
 
 const readUserId = prop('sub');
 
@@ -9,22 +10,40 @@ const throwFailedLoginError = () => {
   throw Boom.unauthorized('Error loging in. Please try again');
 };
 
-const addTokenInfo = accessToken => tokenService
-  .getAccessTokenInfo(accessToken).then(info => ({ accessToken, info }));
+const addTokenInfo = accessToken => tokenService.getAccessTokenInfo(accessToken)
+  .then(info => ({ accessToken, info }));
 
-const getTokenInfoFromCode = code => tokenService
-  .getAccessToken(code)
-  .then(addTokenInfo);
+const getTokenInfoFromCode = code => tokenService.getAccessToken(code).then(addTokenInfo);
 
 const getUserFromAuthId = compose(dataQuery.user.byAuthProviderId, readUserId);
+const registerNewUser = compose(dataMutation.user.register, readUserId);
 
-const login = async (code) => {
+const register = async code => {
   const { accessToken, info } = await getTokenInfoFromCode(code);
+
+  const registerUser = () => registerNewUser(info).then(({ id }) => ({ accessToken, id }));
+
+  const returnAuthDetailsOrRegister = user => (user
+    ? { accessToken, id: user.id }
+    : registerUser());
+
   return getUserFromAuthId(info)
-    .then(user => (user ? { accessToken } : throwFailedLoginError()))
+    .then(returnAuthDetailsOrRegister)
+    .catch(throwFailedLoginError);
+};
+
+const login = async code => {
+  const { accessToken, info } = await getTokenInfoFromCode(code);
+  const returnAuthDetailsOrThrow = user => (user
+    ? { accessToken, id: user.id }
+    : throwFailedLoginError());
+
+  return getUserFromAuthId(info)
+    .then(returnAuthDetailsOrThrow)
     .catch(throwFailedLoginError);
 };
 
 export default {
   login,
+  register,
 };
