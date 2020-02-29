df9bda729a31d9e5444632276b4c8f8bd946cdfd
Paul Vallis <vallis1@gmail.com>
2020-02-29 15:31:06 +0000

    Define theme

--- web/src/theme.js
+++ web/src/theme.js
@@ -1,20 +1,28 @@
-import { linearGradient } from 'polished';
+import { linearGradient, lighten, darken } from 'polished';
 
 const spacingUnit = 0.25;
 
 const primary = '#212e4e';
-const secondary = '#6c372c';
 const highlight = '#54d3e9';
 const shadow = '#1f181d';
+
+const spacing = {
+  unit: spacingUnit,
+  units: multiple => `${multiple * spacingUnit}rem`,
+};
 
 export default {
   colour: {
     primary,
-    secondary,
     highlight,
     shadow,
     neutral: {
       dark: '#111316',
+      light: '#ede9ce',
+    },
+    alert: {
+      confirm: '#63c843',
+      error: '#c84345',
     },
     gradient: {
       primary: linearGradient({
@@ -23,11 +31,18 @@ export default {
         fallback: primary,
       }),
     },
+    softUI: {
+      boxShadow: `box-shadow: ${spacing.units(2)} ${spacing.units(2)} ${spacing.units(4)} ${darken(
+        0.1,
+        primary,
+      )}, ${spacing.units(-2)} ${spacing.units(-2)} ${spacing.units(4)} ${lighten(0.05, primary)}`,
+      activeBoxShadow: `box-shadow: inset ${spacing.units(2)} ${spacing.units(2)} ${spacing.units(4)} ${darken(
+        0.1,
+        primary,
+      )}, inset ${spacing.units(-2)} ${spacing.units(-2)} ${spacing.units(4)} ${lighten(0.05, primary)}`,
+    },
   },
-  spacing: {
-    unit: spacingUnit,
-    units: multiple => `${multiple * spacingUnit}rem`,
-  },
+  spacing,
   typography: {
     fontFamily: 'sans-serif',
   },
